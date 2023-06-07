cb7c00a97357d0ef950dcca71bffa928bf402080
Nicolas Nunez <nicolasnunezromay@gmail.com>
2023-06-07 12:20:07 -0500

    Enhanced image builder, added SSL, added User Registration Service in docker-compose.yml

---
+++ springboot-user-registration-service/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+echo "I am the docker-entrypoint.sh"
+echo "This is my env"
+env
+
+echo "Arguments: $@"
+export SSL_KEYSTORE_LOCATION=${TLS_DIR}/$(hostname)-keystore.jks
+export SSL_TRUSTSTORE_LOCATION=${TLS_DIR}/$(hostname)-keystore.jks
+export SSL_KEY_ALIAS=$(hostname)
+export PRODUCER_CLIENT_ID="producer-$(hostname)"
+sh ${TLS_DIR}/ssl-tls.sh
+
+exec java -jar ${EXECUTABLE_JAR}
