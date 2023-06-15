7570929d69df84f5b63aab8579e3bc96bf5f4890
Nicolas Nunez <nicolasnunezromay@gmail.com>
2023-06-15 14:52:54 -0500

    Pretending people are shopping with python-orders-producer.

--- python-producer/kafka-producer.py
+++ python-producer/kafka-producer.py
@@ -4,6 +4,15 @@ import time
 from confluent_kafka import Producer
 from confluent_kafka.serialization import IntegerSerializer
 import argparse
+import glob
+import os
+import avro.schema
+from avro.io import DatumReader
+from avro.datafile import DataFileReader
+import json
+
+
+
 
 
 def get_argument_parser() -> argparse.ArgumentParser:
@@ -17,7 +26,17 @@ def get_argument_parser() -> argparse.ArgumentParser:
 
 
 def conf_builder(parsed):
-    conf = {'bootstrap.servers': parsed.bootstrap_servers}
+
+    conf = {
+        'bootstrap.servers': parsed.bootstrap_servers,
+        'security.protocol': 'sasl_ssl',
+        'ssl.ca.location': f"{os.getenv('TLS_DIR')}/ca.cert",
+        'ssl.certificate.location': f"{os.getenv('TLS_DIR')}/{hostname}.pem",
+        'ssl.key.location': f"{os.getenv('TLS_DIR')}/{hostname}.key",
+        'sasl.mechanism': 'SCRAM-SHA-512',
+        'sasl.username': f"{os.getenv('SASL_USERNAME')}",
+        'sasl.password': f"{os.getenv('SASL_PASSWORD')}"
+        }
     return conf
 
 
@@ -28,11 +47,49 @@ def get_producer(conf):
     return producer
 
 
-def numbers_data_source():
-    while True:
-        key = random.choice((0, 1, 2, 3))
-        value = random.randrange(1, 2147483647)
-        yield key, value
+def parse_schemas(schema_definition_path=None):
+    if not schema_definition_path:
+        cur_dir = os.getcwd()
+        schema_dir = 'schema/avro'
+        schema_definition_path = os.path.join(cur_dir, schema_dir)
+
+    pattern = '*.avsc'
+    schema_map = {}
+    for filename in glob.glob(f'{schema_definition_path}/{pattern}'):
+        print(filename)
+        base_name = os.path.basename(filename)
+        base = base_name.split('.')[0]
+        with open(filename, 'rb') as fin:
+            schema = avro.schema.parse(fin.read())
+            print(f'Read schema with type: {schema.type} and name: {schema.name}')
+            schema_map[schema.name] = {
+                'schema': schema,
+                'read_path': f'{schema_definition_path}/{base}.avro',
+                'write_path': f'{schema_definition_path}/{base}.avro'
+            }
+    return schema_map
+
+
+def read_orders(schema):
+    with open(schema['read_path'], 'rb') as fin:
+        reader = DataFileReader(fin, DatumReader())
+        for order in reader:
+            yield order['customer_id'], order
+
+
+def generate_orders(schema):
+    path = os.getcwd()
+    filename = os.path.join(path, 'mock-order-data-v1.json')
+    with open(filename) as fin:
+        orders = json.load(fin)
+        with open(schema['write_path'], 'wb') as fout:
+            writer = DataFileWriter(fout, DatumWriter(), schema['schema'])
+            for order in orders:
+                order['create_date'] = int(order['create_date'])
+                order['currency'] = 'USD'
+                # print(order)
+                writer.append(order)
+            writer.close()
 
 
 def delivery_report(err, msg):
@@ -44,25 +101,27 @@ def delivery_report(err, msg):
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 
-if __name__ == '__main__':
-    parser = get_argument_parser()
-    parsed = parser.parse_args(sys.argv[1:])
-    topic = parsed.topic
-    producer = get_producer(conf_builder(parsed))
+def produce_order_events(producer, topic):
+    schemas = parse_schemas()
     integer_serializer = IntegerSerializer()
-    for k, v in numbers_data_source():
+    for k,v in read_orders(schemas['Order']):
         # Trigger any available delivery report callbacks from previous produce() calls
         producer.poll(0)
         # Asynchronously produce a message. The delivery report callback will
         # be triggered from the call to poll() above, or flush() below, when the
         # message has been successfully delivered or failed permanently.
-        producer.produce(topic, key=integer_serializer(k), value=integer_serializer(v), callback=delivery_report)
-        sleepers = random.choice((3, 4, 1, 5, 6))
-        print(f'Sleeping for: {sleepers}')
-        time.sleep(sleepers)
-        # Wait for any outstanding messages to be delivered and delivery report
-        # callbacks to be triggered.
-        producer.flush()
+        value = json.dumps(v).encode('utf-8')
+        producer.produce(topic, key=integer_serializer(k), value=value, callback=delivery_report)
+    producer.flush()
+
+if __name__ == '__main__':
+    parser = get_argument_parser()
+    parsed = parser.parse_args(sys.argv[1:])
+    topic = parsed.topic
+    config = conf_builder(parsed)
+    producer = get_producer(conf_builder(parsed))
+    produce_order_events(producer, topic)
+
 
 
 
